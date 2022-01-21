9644ec63d5d3da54746e61a57bfc9d41a5864ecd
Rosana Pereira <pereira-rosana@outlook.com>
2022-01-21 12:28:45 -0300

    implementação do modal - post

--- target_project/src/components/ModalAddObjective/index.js
+++ target_project/src/components/ModalAddObjective/index.js
@@ -1,22 +1,55 @@
 import "./styles.css";
 import iconClosed from "../../assets/icon_closed.svg";
+import useRequests from "../../helpers/useRequest";
+import { format } from 'date-fns';
 
-function ModalAddObjective ({ setOpen }) {
-    return (
+function ModalAddObjective ({ setOpen, form, setForm, open,setIdd, setReload, reload}) {
+
+    const { post } = useRequests();
+
+    function handleChange(target) {
+        setForm({...form, [target.name]: target.value})
+      }
+      
+     async function handleSubmit(event) {
+        event.preventDefault();
+        if (!form.title || !form.description) {
+            return (
+              alert('Insira os campos: Título e Descrição')
+            )
+          }
+
+
+          const body = {
+            title: form.title, 
+            description: form.description, 
+            category: form.category, 
+            createdAt: format(new Date(), 'MM/dd/yyyy'), 
+            status: "active"
+          };
+        
+        await post("targets", body);
+        setOpen(false)
+        setReload(!reload)
+      }
+
+      return (
         <div className="backdrop">
             <div className="modal">
                 <img src={iconClosed} alt="icon closed" className="modal__icon-close" onClick={() => setOpen(false)} />
 
                 <h1 className="modal__icon-title">Insira o objetivo</h1>
 
-                <form className="modal__form flex-column">
+                <form className="modal__form flex-column" onSubmit={handleSubmit}>
                     <div className="modal__form-txt flex-column mt-2">
                         <label>Título</label>
                         <input 
                             className="mt-1"
                             name="title"
+                            value={form.title}
                             type="text"
                             maxLength="35"
+                            onChange={(event) => handleChange(event.target)}  
                         />
                     </div>
 
@@ -29,6 +62,8 @@ function ModalAddObjective ({ setOpen }) {
                             maxLength="200"
                             name="description"
                             type='text'
+                            value={form.description}
+                            onChange={(event) => handleChange(event.target)} 
                         />
 
                     </div>
@@ -38,7 +73,9 @@ function ModalAddObjective ({ setOpen }) {
                         <input 
                             className="mt-1"
                             name="category"
-                            type='number' 
+                            type='number'
+                            value={form.category}
+                            onChange={(event) => handleChange(event.target)} 
                         />
                     </div>
 
