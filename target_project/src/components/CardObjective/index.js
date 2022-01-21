e97aa54f8677bb7546d05ac5ed1b88f7bd8990c8
Rosana Pereira <pereira-rosana@outlook.com>
2022-01-21 11:09:00 -0300

    implementação da página Delete e do CardObjective

--- target_project/src/components/CardObjective/index.js
+++ target_project/src/components/CardObjective/index.js
@@ -1,55 +1,116 @@
+import { format } from 'date-fns';
+import { useEffect, useState } from "react";
 import IconCheck from "../../assets/icon_check.svg";
 import IconClose from "../../assets/icon_close.svg";
+import useRequests from "../../helpers/useRequest";
+import "../../styles/layout.css";
 import "./styles.css";
 
-function CardObjective({ iconRestore }) {
-    return (
-        <div className={`card__objective mb-2 flex-row justify-between`}>
+function CardObjective ({ iconRestore, 
+    title, description, category, id, status, achievedAt, createdAt, deletedAt,
+    setReload, reload, 
+    setSearch, search }) {
+
+    const { get, patch } = useRequests();
+    const [filterCateg, setFilterCateg] = useState();
+
+    useEffect(() => {
+        if(category){
+        loardCategory()
+        }
+        // eslint-disable-next-line
+    }, [])
+    
+    async function loardCategory() {
+        const results = await get(`categories/${category}`);
+        setFilterCateg(results.name)        
+    }
+      
+    async function handleStatus(id) {
+        const body = {
+            status: "done",
+            achievedAt: format(new Date(), "dd/MM/yyyy")
+        }  
+
+        await patch(`targets/${id}`, body);
+        setReload(!reload)
+        search && setSearch("")
+    }
+
+    async function handleDelete(id){
+        const body = {
+            status: "deleted",
+            deletedAt: format(new Date(), "dd/MM/yyyy")
+        }
+
+        await patch(`targets/${id}`, body);
+        setReload(!reload)
+        search && setSearch("")
+    }
+
+    async function handleRestore(id){
+        const body = {
+            status: "active",
+            createdAt: format(new Date(), "dd/MM/yyyy")
+        }
+
+        await patch(`targets/${id}`, body);
+        setReload(!reload)
+        search && setSearch("")
+    }
+
+    return(
+        <div className={`card__objective mb-2 flex-row justify-between 
+        ${status === "done" && "card__objective-done"} ${iconRestore && "color"}`}>
             <div className="card__objective-text flex-column justify-between">
                 <div>
                     <strong>
-                        test
+                        {title}
                     </strong>
                     <p>
-                        test
+                        {description}
                     </p>
                 </div>
 
                 <div className="card__objective-category flex-column align-center justify-center">
-                    test
+                    {filterCateg}
                 </div>
             </div>
 
-
             {
                 iconRestore ?
-                    <div className="card__objective-icons flex-column">
-                        <img src={iconRestore} alt="icon check"
-                        />
+                <div className="card__objective-icons flex-column align-center"> 
+                    <img src={iconRestore} alt="icon check"
+                    className="mr-5"
+                    onClick={() => handleRestore(id)} />
 
-                        <span>removido em: </span>
+                    <span>removido em: {deletedAt}</span>
+                </div> 
+                
+                :
+            
+                status ==="active" ? 
+                <div className="card__objective-icons flex-column align-center gap-1">
+                    <div className="card__objective-imgs flex-row ">
+                        <img 
+                        src={IconCheck} 
+                        alt="icon check" 
+                        onClick={() => handleStatus(id)} />
+                        <img 
+                        src={IconClose} 
+                        alt="icon check" 
+                        onClick={() => handleDelete(id)}/>
                     </div>
-
-                    :
-
-                    <div className="card__objective-icons flex-column align-center gap-1">
-                        <div className="card__objective-imgs flex-row ">
-                            <img
-                                src={IconCheck}
-                                alt="icon check" />
-                            <img
-                                src={IconClose}
-                                alt="icon check" />
-                        </div>
-                        <span>
-                            criado em:
-                        </span>
-                    </div>
-
+                    <span>
+                        criado em: {createdAt}
+                    </span>
+                </div> 
+                :
+                <span className="card__objective-span flex-row align-self-end">
+                    finalizado em: {achievedAt}
+                </span>
             }
-
-
-        </div>
+    </div>
     )
 }
 
--- target_project/src/pages/Deleted/index.js
+++ target_project/src/pages/Deleted/index.js
@@ -1,16 +1,65 @@
-import "./styles.css";
-import Header from "../../components/Header";
-import CardObjective from "../../components/CardObjective";
+import { useEffect, useState } from "react";
 import iconRestore from "../../assets/icon_restore.svg";
+import CardObjective from "../../components/CardObjective";
+import Header from "../../components/Header";
+import useRequests from "../../helpers/useRequest";
+import "../../styles/layout.css";
+import "./styles.css";
 
 function DeletedObjectives() {
+    const { get } = useRequests();
+    const [resultOfDeletedObjectives, setResultOfDeletedObjectives] = useState()
+    const [search, setSearch] = useState("");
+    const [reload, setReload] = useState(false);
+
+    useEffect(() => {
+        handleLoardObjectives()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [reload])
+
+    useEffect(() => {
+        if (!search) {
+            handleLoardObjectives()
+        } else {
+            const temp = resultOfDeletedObjectives.filter(
+                (objct) =>
+                    objct.title.toLowerCase().includes(search.toLowerCase())
+            );
+
+            setResultOfDeletedObjectives(temp)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [search])
+
+    async function handleLoardObjectives() {
+        const results = await get("targets");
+
+        const filtedDeleted = results.filter(e => e.status === "deleted")
+
+        setResultOfDeletedObjectives(filtedDeleted)
+    }
+
     return (
         <main className="deleted flex-column align-center">
-            <Header />
+            <Header
+                search={search}
+                setSearch={setSearch} />
             <section className="deleted__container flex-column align-center justify-center mt-10">
-
-                <CardObjective 
-                iconRestore={iconRestore}/>
+                {
+                    resultOfDeletedObjectives &&
+                    resultOfDeletedObjectives.map(objct => {
+                        return (
+                            <CardObjective
+                                key={objct.id}
+                                {...objct}
+                                iconRestore={iconRestore}
+                                setReload={setReload}
+                                reload={reload}
+                                setSearch={setSearch}
+                                search={search} />
+                        )
+                    })
+                }
             </section>
         </main>
     );
