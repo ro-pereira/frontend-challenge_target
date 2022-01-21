8a9080433949753c6aa80878b4d3bc369ad61eee
Rosana Pereira <pereira-rosana@outlook.com>
2022-01-21 11:12:17 -0300

    Implementação da página Objectives

--- target_project/src/pages/Objective/index.js
+++ target_project/src/pages/Objective/index.js
@@ -1,33 +1,93 @@
-import { useState } from "react";
+import { format } from 'date-fns';
+import { useEffect, useState } from "react";
 import CardObjective from "../../components/CardObjective";
 import Header from "../../components/Header";
 import ModalAddObjective from "../../components/ModalAddObjective";
+import useRequests from "../../helpers/useRequest";
 import "./styles.css";
 
 function Objectives() {
-    const [open, setOpen] = useState(false);
+  const { get } = useRequests();
+  
+  const [open, setOpen] = useState(false);
+  const [form, setForm] = useState({ title: "", 
+  description: "", 
+  category: "", 
+  createdAt: format(new Date(), 'MM/dd/yyyy'), 
+  status: "active" })
+  const [reload, setReload] = useState(false);
+  const [resultsObjective, setResultsObjective] = useState()
+  const [search, setSearch] = useState("")
 
-    return (
-        <main className="objectives flex-column align-center">
-          <Header />
-    
-          <section className="objectives__container flex-column mt-3">
-            <button className="objectives__container-button mb-3 align-self-end"
-            onClick={() => setOpen(true)}>
-              Novo objetivo
-            </button>
-            
-            <CardObjective />
-
-            {
-            open && <ModalAddObjective 
-                    className="mt-2"
-                    setOpen={setOpen} />   
-            }
-          
-          </section>
-        </main>
+  useEffect(() => {
+  if(!search){
+    loardObjectives()
+  } else {
+      const temp = resultsObjective.filter(
+      (objective) =>
+        objective.title.toLowerCase().includes(search.toLowerCase())
       );
+      
+      setResultsObjective(temp)
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search])
+
+  useEffect(() => {
+    loardObjectives()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload])
+
+  
+  async function loardObjectives() {
+    const results = await get("targets");
+
+    const filterActives = results.filter(e => e.status === "active" || e.status === "done" )
+
+    setResultsObjective(filterActives)
+  }
+
+  return (
+    <main className="objectives flex-column align-center">
+      <Header 
+      search={search} 
+      setSearch={setSearch} />
+
+      <section className="objectives__container flex-column mt-3">
+        <button className="objectives__container-button mb-3 align-self-end" 
+        onClick={() => setOpen(true)}>
+          Novo objetivo
+        </button>
+        
+        {
+          resultsObjective &&
+          resultsObjective.map(e => {
+            return (
+              <CardObjective 
+              {...e} 
+              key={e.id}
+              setReload={setReload}
+              reload={reload} 
+              search={search}
+              setSearch={setSearch}
+              /> 
+            )
+          })
+        }
+        
+        {
+         open && <ModalAddObjective 
+                  className="mt-2"
+                  setOpen={setOpen}
+                  setForm={setForm}
+                  form={form}
+                  open={open}
+                  setReload={setReload}
+                  reload={reload} />   
+        }
+      </section>
+    </main>
+  );
 }
 
 export default Objectives; 
